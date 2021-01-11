c26a3ff3a3276b46107519dad1a2711366e5eca6
Cherno Badjan <badjancherno@gmail.com>
2021-01-11 14:17:42 -0800

    button test

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // import functions and grab DOM elements
+const submitButton = document.getElementById('submit-button');
+const resultsSection = document.getElementById('results-section');
 
 // initialize state
 
+
 // set event listeners to update state and DOM
+submitButton.addEventListener('click', () => {
+    console.log('Working)')
+});
