f480614e3f769fa4212ac15107c19c1418990c15
Cherno Badjan <badjancherno@gmail.com>
2021-01-11 17:23:43 -0800

    first test

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,17 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { rightAnswer } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('rightAnswer should return true when fed a y/Y', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = rightAnswer('Yes');
 
     //Expect
     // Make assertions about what is expected versus the actual result
