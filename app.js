ae347543777bdb4935fd72ea389a47cdd0ca43d1
Cherno Badjan <badjancherno@gmail.com>
2021-01-11 16:40:37 -0800

    styling complete

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // import functions and grab DOM elements
+import { rightAnswer } from './utils.js';
 const submitButton = document.getElementById('submit-button');
 const resultsSection = document.getElementById('results-section');
-
-// initialize state
 
 
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
-    console.log('Working)')
+    alert('Welcome to the Ordinary Wizarding TriQuestion Quiz!');
+
+    const proceedMessage = confirm('Are you ready to start the TriQuestion Quiz?');
+    if (!proceedMessage) return;
+
+    let rightAnswer = 0;
+
+
+
+    const userName = prompt('What is your name');
+    const answerOne = prompt('Is Harry a Muggle?');
+    const answerTwo = prompt('Is Hagrid the boy who lived?');
+    const answerThree = prompt('Is Dumbledore also known as Tom Marvolo Riddle?');
+
+
+
+
+
 });
---
+++ utils.js
@@ -0,0 +1,3 @@
+export function (rightAnswer) {
+    return answer.charAt(0).toUpperCase() === 'Y'
+};
